332954fa73f5eda999918a07b33bc4d3c3644a69
MRAM96 <mramsk96@gmail.com>
2025-08-25 00:15:00 +0530

    Added Day1: practice and challenge files

---
+++ Day1/day1_practice.py
@@ -0,0 +1,29 @@
+name="bro"
+age=22
+country="india"
+is_student=True
+print("Name:",name)
+print("Age:",age)
+print("Country:",country)
+print("Student:",is_student)
+
+
+fav_no=[27,31,96]
+total=sum(fav_no)
+average=total/len(fav_no)
+print("Lucky number:",fav_no)
+print("Sum:",total)
+print("Average:",average)
+
+
+
+
+skill={
+    "python":"Beginner",
+    "Ml":"Beginner",
+    "Quantum":"Beginner"
+    }
+print("My Skill:",skill)
+
+
+
